625fbfb445df01157da89381e37ea184d358deee
Dante <elpinguinoalan@gmail.com>
2025-06-30 16:01:01 -0300

    Web => Chartjs 2 Added (with charts) after a year :p | v1.7

--- Web - React/src/app/pages/robot.jsx
+++ Web - React/src/app/pages/robot.jsx
@@ -1,34 +1,19 @@
 import '../../styles/routes_styles/general.routes.styles.css'
 import '../../styles/routes_styles/robot.css'
-
-import AldoRobot from '/icons/nav_icons/robot.svg'
-
+import "../../styles/Charts.css"
+import { PieChart } from '../components/PieChart.tsx'
+import { BarChart } from '../components/BarChart.tsx'
 
 export function Robot(){
-  return(
-    <div className="banner-container">
-      <Banner
-        bannerIcon={AldoRobot}
-        bannerAlt="Name"
-        robotName="Aldo"
-      />
-
-      <Banner
-        bannerIcon={AldoRobot}
-        bannerAlt="Name"
-        robotName="Jamón"
-      />
-      
+  return (
+  <div className="main-box">
+    <div className="wrapper">
+        <div className="one chart-style chart-view-sm">
+          <PieChart />
+        </div>
+        <div className="two chart-style chart-view-sm">
+          <BarChart />
+        </div>
     </div>
-  )
-} 
-
-
-function Banner({bannerIcon,bannerAlt,robotName}){
-  return(
-    <div className="banner">
-      <h2 className='banner-text'>{robotName}</h2>
-      <img className='banner-img' src={bannerIcon} alt={bannerAlt} />
-    </div>
-  )
-}
+  </div>
+  )}
